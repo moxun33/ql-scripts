bd86cb770cf90568b6410939c24cf96e250a2fa1
moxun33 <mxx0807@gmail.com>
2022-06-12 00:42:03 +0800

    f

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo '开始部署real-url 数据到vercel。'
 cd moxun33_ql-scripts_main
-node live_douyu.js
-node live_huya.js
-node live_bilibili.js
+node douyu.js
+node huya.js
+node bilibili.js
 cd data
 pnpm exec vercel -t ${VERCEL_TOKEN}
 echo '部署real-url 数据到vercel完毕。'
