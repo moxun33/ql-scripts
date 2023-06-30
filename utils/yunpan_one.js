e97078aa360d941463fa57fd5f210a3c4f2acfd0
moxun33 <mxx0807@gmail.com>
2023-06-30 11:31:33 +0800

    :bug: Fixing a bug.

--- utils/yunpan_one.js
+++ utils/yunpan_one.js
@@ -12,16 +12,16 @@ class YunpanOne {
   origin = "https://yunpan1.cc";
 
   //获取回复列表
-  async getComments(page = 1, limit = 20) {
+  async getComments(offset = 0, limit = 20) {
     try {
       const params = {
         "filter[author]": this.user,
         "filter[type]": "comment",
-        "page[offset]": page ?? 1,
+        "page[offset]": offset ?? 0,
         "page[limit]": limit ?? 20,
         sort: "-createdAt",
       };
-      return fireFetch(this.origin + "/api/posts" + genUrlSearch(params),{}, true);
+       return fireFetch(this.origin + "/api/posts" + genUrlSearch(params),{}, true);
     } catch (e) {
       console.error(e);
       return {};
@@ -37,6 +37,7 @@ class YunpanOne {
 
         e.attributes?.contentHtml?.includes("总合集【更新中的】和完结的")
     );
+   // console.log(JSON.stringify(data))
     const target = filtered?.length ? filtered[0] : {};
      if (!target) return "";
     //第一个阿里云盘分享链接就是总链接
